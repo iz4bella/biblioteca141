1496fd51f6fc696b654306a0649ad99dcaa9dc4d
iz4bella <izabellac161@gmail.com>
2024-11-05 10:51:04 -0300

    header

---
+++ template/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</Cadastro>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav class="container">
+            <div class="logo-box">
+                <a href= "youtube_logo.png" target="_blank">
+                    <img src=icone.png alt="blablabla" class="logo">
+                </a>
+            </div>
+            <div class="links">
+                <a href="#" class="social-links">sobre</a>
+                <a href="#" class="social-links">projetos</a>
+                <a href="#" class="social-links">experiencia</a>
+                <a href="#" class="social-links">contato</a>
+            </div>
+            <div class="icon">
+                <a href= "ig_icone.png" target="_blank" class="sm-icon">
+                    <ion-icon name="logo instagram"></ion-icon>
+                </a>
+                <a href= "twt.png" target="_blank" class="sm-icon">
+                    <ion-icon name="logo twitter"></ion-icon>
+                </a>
+                <a href= "github.png" target="_blank" class="sm-icon">
+                    <ion-icon name="logo github"></ion-icon>
+                </a>
+            </div>
+        </nav>
+    </header>
