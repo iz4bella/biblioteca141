55722df5ca8bf6ca758ce8d22f515ca82ff01389
iz4bella <izabellac161@gmail.com>
2024-11-06 09:41:25 -0300

    arrumando o login

--- login.php
+++ login.php
@@ -7,57 +7,58 @@
     <link rel="stylesheet" href="./views/style.css">
 </head>
 <body>
+    <!-- Início do Cabeçalho -->
     <header>
         <nav class="container">
+            <!-- Logo -->
             <div class="logo-box">
-                <a href="./imgs/icone.png" target="_blank">
-                    <img src="./imgs/icone.png" alt="blablabla" class="logo">
+                <a href="./index.php">
+                    <img src="./imgs/icone.png" alt="Logo" class="logo">
                 </a>
             </div>
+            <!-- Links de navegação -->
             <div class="parent-link">
                 <a href="#" class="social-links">Sobre</a>
                 <a href="#" class="social-links">Projetos</a>
                 <a href="#" class="social-links">Experiência</a>
                 <a href="#" class="social-links">Contato</a>
             </div>
-            <div class="icon">
-                <a href="" target="_blank" class="sm-icon">
-                    <img src="./imgs/ig_icone.png" alt="" srcset="">
-                </a>
-                <a href="" target="_blank" class="sm-icon">
-                    <img src="./imgs/Twitter-Logo.png" alt="" srcset="">
-                </a>
-                <a href="" target="_blank" class="sm-icon">
-                    <img src="./imgs/github.png" alt="" srcset="">
-                </a>
+            <!-- Ícones sociais -->
+            <div class="social-icons">
+                <a href="#" class="social-icon"><img src="./imgs/ig_icone.png" alt="Instagram"></a>
+                <a href="#" class="social-icon"><img src="./imgs/Twitter-Logo.png" alt="Twitter"></a>
+                <a href="#" class="social-icon"><img src="./imgs/github.png" alt="GitHub"></a>
             </div>
         </nav>
     </header>
+    <!-- Fim do Cabeçalho -->
+
+    <!-- Formulário de Login -->
     <div class="form">
         <form action="#" class="sub-form">
             <div class="upper-form">
-                <h2>Formulario login</h2>
-                <label for="nome">Nome</label><br>
-                <input type="text" id="nome" name="Nome"><br>
-                <label for="senha">Senha</label><br>
-                <input type="password" id="senha" name="Senha"><br>
-                    <a href="home.php" class="Login">Login</a>
+                <h2>Formulario de Login</h2>
 
-                    
-                </div>
+                <!-- Campo de Nome -->
+                <label for="nome">Nome</label>
+                <input type="text" id="nome" name="Nome" required><br>
+
+                <!-- Campo de Senha -->
+                <label for="senha">Senha</label>
+                <input type="password" id="senha" name="Senha" required><br>
+
+                <!-- Link para o botão de Login -->
+                <a href="home.php" class="custom-link">Login</a>
             </div>
+
+            <!-- Botão de Voltar -->
             <a href="index.php" class="voltar">Voltar</a>
+
+            <!-- Seção de Cadastrar-se -->
             <div class="button-form">
-                <div class="no-account">
-                    Você não tem conta
-                    <a href="#" class="Logar">Cadastrar-se</a>
-                </div>
-                
+                <p>Você não tem conta? <a href="#" class="Logar">Cadastrar-se</a></p>
             </div>
         </form>
-        
     </div>           
 </body>
 </html>
-
-<?php
